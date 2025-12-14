2ad4ea93abdda59ed5b8697d0dcf6e9be709482a
daegon <adilsondantas2015@gmail.com>
2025-12-14 12:13:12 -0300

    Add initial appState object to state.js

---
+++ public/js/state.js
@@ -0,0 +1,14 @@
+export const appState = {
+  activeChart: "songs",
+  currentDate: null,
+  currentYear: new Date().getFullYear().toString(),
+
+  chartData: {},
+  colMaps: {},
+  top3Data: {},
+
+  chartRunData: {},
+  chartBeatData: {},
+
+  imageCache: {}
+};
