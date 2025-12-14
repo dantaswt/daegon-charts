22993900fc5b9a1e1abab8c88da85028223b8eef
daegon <adilsondantas2015@gmail.com>
2025-12-14 12:13:48 -0300

    Add charts configuration for songs, artists, and albums

---
+++ public/js/config.charts.js
@@ -0,0 +1,43 @@
+export const chartsConfig = {
+  songs: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=904867620",
+    title: "Hot 100",
+    entityType: "song"
+  },
+  artists: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=1568177610",
+    title: "Artist 50",
+    entityType: "artist"
+  },
+  albums: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=1940039611",
+    title: "Top 100 Albums",
+    entityType: "album"
+  },
+
+  yearEndSongs: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=530686468",
+    title: "Year-End Songs"
+  },
+  yearEndArtists: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=1597569311",
+    title: "Year-End Artists"
+  },
+  yearEndAlbums: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=897935603",
+    title: "Year-End Albums"
+  },
+
+  goatSongs: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=1157278896",
+    title: "GOAT Songs"
+  },
+  goatArtists: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=222299678",
+    title: "GOAT Artists"
+  },
+  goatAlbums: {
+    url: "https://docs.google.com/spreadsheets/d/1t6_7SOlspmNYrXq8PSfJ74frIdrWwQBFITQ3bQmRzeg/export?format=csv&gid=1548244755",
+    title: "GOAT Albums"
+  }
+};
